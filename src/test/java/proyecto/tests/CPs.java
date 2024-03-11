d120c5ab2c19d38a4ebddf95b45b05c2b5573bf1
BrayanTorres <brayantorresacuna2023@gmail.com>
2024-03-11 16:04:17 -0500

    Agregar caso de prueba TC001 de la funcionalidad de vuelos

--- src/test/java/proyecto/tests/CPs.java
+++ src/test/java/proyecto/tests/CPs.java
@@ -8,10 +8,12 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import proyecto.pages.HomePage;
+import proyecto.pages.VuelosPage;
 
 public class CPs {
 
     HomePage home;
+    VuelosPage vuelosPage;
     WebDriver driver;
 
     @BeforeAll
@@ -24,6 +26,7 @@ public class CPs {
         driver = new ChromeDriver();
         home = new HomePage(driver);
         home.cargarSitio("https://www.rumbo.es/");
+        home.maximizarBrowser();
     }
 
     @AfterEach
@@ -33,6 +36,33 @@ public class CPs {
 
     @Test
     public void TC001_Busqueda_Vuelos_Baratos_Europa_IdaYVuelta_Campos_Vacios() {
+        //Aceptar cookies
+        home.aceptarCookies();
 
+        home.esperarXsegundos(1000);
+        home.irAVuelos();
+        vuelosPage = new VuelosPage(driver);
+        vuelosPage.hacerScrollHastaCategoriaVuelos();
+        vuelosPage.irAVuelosAEuropa();
+        vuelosPage.esperarXsegundos(5000);
+        vuelosPage.cambiarALaUltimaVentanaAbierta();
+        vuelosPage.esperarXsegundos(1000);
+        vuelosPage.seleccionarOpcionIdaYVuelta();
+
+        try {
+            vuelosPage.limpiarValorOrigenPorDefecto();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        vuelosPage.ingresarOrigenVuelo("");
+        vuelosPage.esperarXsegundos(1000);
+        vuelosPage.ingresarDestinoVuelo("");
+        vuelosPage.esperarXsegundos(1000);
+        vuelosPage.buscarVuelos();
+        vuelosPage.esperarXsegundos(3000);
+        //Validar resultados
+        vuelosPage.validarCampoOrigen();
+        vuelosPage.validarCampoDestino();
     }
 }
